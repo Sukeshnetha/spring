5484b5c6e743742f54e397de02ab6e29312b493b
sukesh <sukeshnetha74@gmail.com>
2024-11-08 13:28:35 +0530

    commit message

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class DemoController {
+    @GetMapping("/")
+    public String getwelcometext(){
+        return "Hello world";
+    }
+    @GetMapping("/suk")
+    public String getMethodName() {
+        return "Erripuka";
+    }
+    
+}
